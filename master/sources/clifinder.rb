3eb655be214d9f37e1b0d26f649499c14c25202c
swalf <swalf@tiscali.it>
2014-11-03 09:38:19 +0100

    Added code for reconstruct json from a line.

--- master/sources/clifinder.rb
+++ master/sources/clifinder.rb
@@ -1,5 +1,6 @@
 require "json"
 require "elasticsearch"
+require_relative "indexercodes.rb"
 
 class CLIFinder
 
@@ -14,20 +15,25 @@ class CLIFinder
         answers = results["hits"]["hits"].inject([]) {|stor, el| [el["_source"]]}
         puts "#{answers.length} result(s) found:"
         answers.each_with_index do |answer,i|
-            puts "#{i}:"
+            puts "#{i+1}:"
             answer.each_pair {|key, value| (puts key + ': ' + value.to_s) unless key == "position"}
             puts "Document:"
             filepath = answer["position"]["dir"] + '/' + answer["position"]["name"] + answer["position"]["extension"]
             File.open(filepath, "r") do |file|
                 file.seek answer["position"]["line_start_byte"].to_i
                 line = file.gets
-                line.length.times {print '#'}
                 puts
                 puts line
-                line.length.times {print '#'}
                 puts
+                obj = self.reconstruct(line, answer["position"]["extension"])
+                puts obj
             end
         end
+    end
+    
+    def reconstruct (line, type)
+        mn = tipe[1..-1].capitalize + "_code"
+        ReconstructorCodes.const_get(mn).call line, type
     end
 
 end
--- master/test/indexertest.rb
+++ master/test/indexertest.rb
@@ -8,12 +8,19 @@ require 'indexer'
 require 'elasticsearch'
 
 class Test_indexer < Test::Unit::TestCase
-  def test_index
+    
+#  def test_index
+#    test_client = Elasticsearch::Client.new log: true
+#    test_indexer = Indexer.new test_client "test_index"
+#    sample_files = (Dir.entries "./samplefiles/").select {|el| File(el).file?}
+#    sample_files.each {|filepath| test_indexer.index filepath}
+#    test_client.search index: "test_index", q: query_text   
+#    # assert_equal("foo", bar)
+#  end
+  
+  def test_cliquery
     test_client = Elasticsearch::Client.new log: true
-    test_indexer = Indexer.new test_client "test_index"
-    sample_files = (Dir.entries "./samplefiles/").select {|el| File(el).file?}
-    sample_files.each {|filepath| test_indexer.index filepath}
-        
-    # assert_equal("foo", bar)
+    test_clif = CLIFinder.new test_client "a3"
+    test_clif.query "TSS8"
   end
 end
