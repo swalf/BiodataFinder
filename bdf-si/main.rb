7dd6a280aa6fd8554158ff1c143132bfcc4d1c96
swalf <swalf@tiscali.it>
2014-11-28 15:04:52 +0100

    Some SI file separated

--- bdf-si/main.rb
+++ bdf-si/main.rb
@@ -29,69 +29,24 @@ get '/search' do
     slim :search
 end
 
-post '/searched' do
+post '/results' do
     @title = "You have search:"
     @searched = params[:search].to_s
     client = (Elasticsearch::Client.new)
-    index = "blog"
+    index = "a3"
     finder = Finder.new(client, index)
     query_text = @searched
-    @results = (finder.query query_text, 'pretty_json')
-    slim :searched
+    results = (finder.query query_text, 'pretty_json')
+    @str_res = ""
+    @str_res << "#{results[:gen_infos][:nres]} result(s) found (max score #{results[:gen_infos][:max_scores]}):\n"
+    results[:objs].each_with_index do |obj,i|
+        @str_res << "Result #{i+1} (score #{obj[:infos][:scores]}):\n"
+        @str_res << obj[:data]
+    end
+    slim :results
 end
 
 not_found do
     erb :not_found
 end
-=begin
-  get '/search/:index/:text' do
-    client = Elasticsearch::Client.new log: true
-    index = params[:index]
-    #index = @def_index if index.nil?
-    #raise "If you don't specify an espicit index, you have to set default index key with setdef command." if index.nil?
-    finder = Finder.new(client, index)
-    query_text = params[:text].split('_').join(' ')
-    res = (finder.query query_text, 'rawline')
-    res
-  end
-=end  
-
-
-__END__
-@@layout
-doctype html
-html lang="en"
-  head
-    title== @title || "BiodataFinder"
-    meta charset="utf-8"
-    link rel="stylesheet" href="/styles.css"
-  body
-    header
-      h1 BiodataFinder
-      h2== @title || "Boh!"
-      nav
-        ul
-          li <a href="/" title="Home">Home</a>
-          li <a href="/search" title="Search">Search</a>
-          li <a href="/about" title="About">About</a>
-    section
-      == yield
-
-@@home
-p Welcome in the alpha stage BDF web interface for searching.
-
-
-@@searched
-p Theese are the results of your search:
-p== @results
-p Enjoy your life!
-
-@@about
-p This software is written by Alessandro Bonfanti and licenced under GNU GPLv3
-
-@@not_found
-h2 404!
-p The page you are looking for is missing. Why not go back to the <a href='/'>home page</a> and start over?
-
-
 
