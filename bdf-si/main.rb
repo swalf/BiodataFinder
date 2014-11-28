897db5cca1434760f5c996d5289892dde2754fc7
swalf <swalf@tiscali.it>
2014-11-28 12:28:38 +0100

    Restructured Sinatra interface

--- bdf-si/main.rb
+++ bdf-si/main.rb
@@ -4,7 +4,11 @@
 require 'sinatra'
 require 'slim'
 require 'sinatra/reloader' if development?
-#require 'finder'
+if development?
+  require_relative '../lib/bdf-finder.rb'
+else
+  require 'finder'
+end
 
 configure do
     set :port, 1234
@@ -12,15 +16,28 @@ configure do
 end
 
 get '/' do
-    erb :home
+    @title = "A tool for searching in biodata files."
+    slim :home
 end
 
 get '/about' do
-    erb :about
+    slim :about
 end
 get '/search' do
+    @title = "Enter a new search!"
     @search
     slim :search
+end
+
+post '/searched' do
+    @title = "You have search:"
+    @searched = params[:search].to_s
+    client = (Elasticsearch::Client.new)
+    index = "blog"
+    finder = Finder.new(client, index)
+    query_text = @searched
+    @results = (finder.query query_text, 'pretty_json')
+    slim :searched
 end
 
 not_found do
@@ -42,42 +59,39 @@ end
 
 __END__
 @@layout
-<% title="BiodataFinder" %>
-<!doctype html>
-<html lang="en">
-    <head>
-        <title><%= title %></title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <header>
-            <h1><%= title %></h1>
-            <nav>
-                <ul>
-                    <li><a href="/" title="Home">Home</a></li>
-                    <li><a href="/about" title="About">About</a></li>
-                    <li><a href="/search" title="Search">Search</a></li>
-                </ul>
-            </nav>
-        </header>
-        <section>
-            <%= yield %>
-        </section>
-    </body>
-</html>
+doctype html
+html lang="en"
+  head
+    title== @title || "BiodataFinder"
+    meta charset="utf-8"
+    link rel="stylesheet" href="/styles.css"
+  body
+    header
+      h1 BiodataFinder
+      h2== @title || "Boh!"
+      nav
+        ul
+          li <a href="/" title="Home">Home</a>
+          li <a href="/search" title="Search">Search</a>
+          li <a href="/about" title="About">About</a>
+    section
+      == yield
 
 @@home
-<p>Welcome in the alpha stage BDF web interface for searching.</p>
-<img src="/images/13671.gif" alt="BDF-img">
+p Welcome in the alpha stage BDF web interface for searching.
+
+
+@@searched
+p Theese are the results of your search:
+p== @results
+p Enjoy your life!
 
 @@about
-<p>This software is written by Alessandro Bonfanti and licenced under GNU GPLv3</p>
+p This software is written by Alessandro Bonfanti and licenced under GNU GPLv3
 
 @@not_found
-<h2>4 Oh 4!</h2>
-<p> The page you are looking for is missing. Why not go back to the
-<a href='/'>home page</a> and start over?</p>
+h2 404!
+p The page you are looking for is missing. Why not go back to the <a href='/'>home page</a> and start over?
 
 
 
